80bdddf7833874fba5d9d1a86e629440b3d60079
Corentinrmt <corentin.remot@gmail.com>
2024-01-16 10:48:18 +0100

    change logo size and setting script for music volume

--- Assets/Scripts/Corentin/MenuScripts/SettingsScript.cs
+++ Assets/Scripts/Corentin/MenuScripts/SettingsScript.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour
 {
+    [SerializeField] private Slider _musicSlider;
+
+    public void SetNewMusicVolume()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", _musicSlider.value);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,7 @@ public class SettingsScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
