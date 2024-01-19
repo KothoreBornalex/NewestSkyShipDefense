d0893a18c3d109e5538281beefcadb4e7a578916
Corentinrmt <corentin.remot@gmail.com>
2024-01-19 23:10:34 +0100

    settings in game

--- Assets/Scripts/Corentin/MenuScripts/SettingsScript.cs
+++ Assets/Scripts/Corentin/MenuScripts/SettingsScript.cs
@@ -8,11 +8,27 @@ public class SettingsScript : MonoBehaviour
     [SerializeField] private Slider _musicSlider;
     [SerializeField] private Slider _effectsSlider;
 
-    public void SetNewMusicVolume()
+    private float _resumeTimeScale;
+
+    public void SetNewVolume()
     {
         PlayerPrefs.SetFloat("MusicVolume", _musicSlider.value);
         PlayerPrefs.SetFloat("EffectsVolume", _effectsSlider.value);
     }
+    public void PauseGame()
+    {
+        _resumeTimeScale = Time.timeScale;
+        Time.timeScale = 0.0f;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = _resumeTimeScale;
+    }
+    public void ResetTimeScale()
+    {
+        Time.timeScale = 1.0f;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
